7fe163e4a52f88a001abb38d868c3ed440824b90
ginguene <ginguene@memnon>
2017-09-21 20:09:38 +0200

    pb protocole

--- src/main/java/fr/ginguene/onepoint/bot/Response.java
+++ src/main/java/fr/ginguene/onepoint/bot/Response.java
@@ -7,26 +7,25 @@ import java.util.StringJoiner;
 import fr.ginguene.onepoint.bot.ordre.Ordre;
 
 public class Response {
-	
+
 	private List<Ordre> ordres = new ArrayList<Ordre>();
-	
-	public void addOrdre(Ordre ordre){
+
+	public void addOrdre(Ordre ordre) {
 		ordres.add(ordre);
 	}
-	
-	public  List<Ordre> getOrdres(){
+
+	public List<Ordre> getOrdres() {
 		return this.ordres;
 	}
-	
-	
-	public String toString(){
-		
-		System.out.println("Nb Ordres:" +  ordres.size());
-		 StringJoiner joiner = new StringJoiner("\n");
-		 for (Ordre ordre: ordres){
-			 joiner.add(ordre.toString());
-		 }
-		 
+
+	public String toString() {
+
+		System.out.println("Nb Ordres:" + ordres.size());
+		StringJoiner joiner = new StringJoiner("\n");
+		for (Ordre ordre : ordres) {
+			joiner.add(ordre.asString());
+		}
+
 		return joiner.toString();
 	}
 
