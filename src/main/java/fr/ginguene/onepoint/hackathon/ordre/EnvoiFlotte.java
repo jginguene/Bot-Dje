661bbfd83779fa638c7eaa0dca31db2d1cdb1ee0
ginguene <ginguene@memnon>
2017-09-24 21:44:42 +0200

    Ajout de réègles métiers

--- src/main/java/fr/ginguene/onepoint/hackathon/ordre/EnvoiFlotte.java
+++ src/main/java/fr/ginguene/onepoint/hackathon/ordre/EnvoiFlotte.java
@@ -28,6 +28,13 @@ public class EnvoiFlotte implements Ordre {
 	public String asString() {
 
 		if (flotte.getVaisseaux() <= 3) {
+			System.err.println("Essai d'envoi d'une flotte de " + flotte.getVaisseaux() + " vaisseaux (<3) de "
+					+ flotte.getPlaneteDestination() + " vers " + flotte.getPlaneteDestination());
+			return "";
+		}
+
+		if (flotte.getPlaneteDestination() == flotte.getPlaneteSource()) {
+			System.err.println("Essai d'envoi d'une flotte depuis et vers  " + flotte.getPlaneteSource());
 			return "";
 		}
 
