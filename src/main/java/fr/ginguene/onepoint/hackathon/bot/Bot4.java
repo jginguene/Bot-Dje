c6d401ce0a9d0610350e427c800ebe4acec5bad5
ginguene <ginguene@memnon>
2017-09-24 21:38:55 +0200

    amélioration stratégie

--- src/main/java/fr/ginguene/onepoint/hackathon/bot/Bot4.java
+++ src/main/java/fr/ginguene/onepoint/hackathon/bot/Bot4.java
@@ -1,8 +1,6 @@
 package fr.ginguene.onepoint.hackathon.bot;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import fr.ginguene.onepoint.hackathon.Carte;
 import fr.ginguene.onepoint.hackathon.Constantes;
@@ -15,54 +13,6 @@ public class Bot4 implements IBot {
 
 	private BotPremierTour botPremierTour = new BotPremierTour();
 	private BotSecondTour botSecondTour = new BotSecondTour();
-
-	private Planete lastDefaultDestination = null;
-
-	private Planete getDefaultDestination(Carte carte) {
-
-		if (lastDefaultDestination != null) {
-			if (lastDefaultDestination.getPopulation() + carte.getFlottesEnnemies(lastDefaultDestination.getId())
-					- carte.getMesFlottes(lastDefaultDestination.getId()) < -50) {
-				return lastDefaultDestination;
-			}
-
-		}
-
-		// 1er tri par ditance;
-		Map<Planete, Integer> planeteDistance = new HashMap<>();
-		for (Planete aPlanete : carte.getPlanetesEtrangere()) {
-			for (Planete myPlanete : carte.getMesPlanetes()) {
-				if (!planeteDistance.containsKey(aPlanete)) {
-					planeteDistance.put(aPlanete, 0);
-				}
-				planeteDistance.put(aPlanete,
-						planeteDistance.get(aPlanete) + carte.getTrajetNbTour(myPlanete, aPlanete));
-
-			}
-		}
-
-		lastDefaultDestination = null;
-
-		int minScore = 0;
-		System.out.println("==>carte:" + carte);
-		System.out.println("==>carte.getPlanetesEtrangere():" + carte.getPlanetesEtrangere());
-		for (Planete aPlanete : carte.getPlanetesEtrangere()) {
-			int score = aPlanete.getPopulation() + planeteDistance.get(aPlanete) / carte.getMesPlanetes().size()
-					+ carte.getFlottesEnnemies(aPlanete.getId());
-
-			System.out.println(aPlanete + "==>" + planeteDistance.get(aPlanete) + "#" + aPlanete.getPopulation() + "#"
-					+ carte.getFlottesEnnemies(aPlanete.getId()));
-
-			if (score < minScore || lastDefaultDestination == null) {
-				lastDefaultDestination = aPlanete;
-				minScore = score;
-			}
-
-		}
-
-		return lastDefaultDestination;
-
-	}
 
 	private Planete getDestinationForBomb(Carte carte, Planete source, int bombSize) {
 
@@ -88,47 +38,9 @@ public class Bot4 implements IBot {
 
 		List<Planete> mesPlanetes = carte.getPlanetes(Constantes.MOI);
 
-		Planete defaultDestination = this.getDefaultDestination(carte);
-
 		for (Planete source : mesPlanetes) {
 
-			int i = 0;
-
-			// Démarrage
-			/*
-			 * if (carte.getConfiguration().getTour() < 20) { for (Planete
-			 * aPlanete : carte.getPlanetesOrderByDistance(source)) { if
-			 * (aPlanete.getProprietaire() != Constantes.MOI && i < 5) { int
-			 * nbSentVaisseau = carte.getMesFlottes(aPlanete.getId());
-			 * 
-			 * if (aPlanete.getPopulation() > nbSentVaisseau &&
-			 * aPlanete.getPopulation() < source.getPopulation() +
-			 * nbSentVaisseau + 1) {
-			 * 
-			 * int nbVaisseau = 1 + aPlanete.getPopulation() - nbSentVaisseau;
-			 * 
-			 * if (nbVaisseau >= source.getPopulation()) { nbVaisseau =
-			 * source.getPopulation() - 1; }
-			 * 
-			 * Flotte flotte = new Flotte();
-			 * flotte.setPlaneteDestination(aPlanete.getId());
-			 * 
-			 * flotte.setVaisseaux(nbVaisseau);
-			 * flotte.setPlaneteSource(source.getId());
-			 * 
-			 * EnvoiFlotte ordre = new EnvoiFlotte(flotte);
-			 * source.remPopulation(nbVaisseau); response.addOrdre(ordre);
-			 * carte.addFlotte(flotte); }
-			 * 
-			 * i++; } break;
-			 * 
-			 * }
-			 * 
-			 * }
-			 */
-
 			if (source.getPopulation() > 5) {
-				Planete destination = defaultDestination;
 
 				// Mode territoire ennemi
 				// On ne fait rien dans ce cas là
@@ -150,7 +62,27 @@ public class Bot4 implements IBot {
 					nbVaisseau = 0;
 					if (source.getPopulation() > 140) {
 						nbVaisseau = source.getPopulation() / 2;
-						destination = getDestinationForBomb(carte, source, nbVaisseau);
+						Planete destination = getDestinationForBomb(carte, source, nbVaisseau);
+						EnvoiFlotte ordre = new EnvoiFlotte(source, destination, nbVaisseau);
+						source.remPopulation(nbVaisseau);
+						response.addOrdre(ordre);
+						carte.addFlotte(ordre.getFlotte());
+						break;
+					}
+
+				}
+
+				// Cas standards;
+				Planete destination = null;
+				int minScore = 0;
+				for (Planete aPlanete : carte.getPlanetesEtrangeres()) {
+
+					int aScore = carte.getTrajetNbTour(source, aPlanete) * 2 + aPlanete.getPopulation()
+							- carte.getMesFlottes(aPlanete.getId());
+
+					if (aScore < minScore || destination == null) {
+						destination = aPlanete;
+						minScore = aScore;
 					}
 
 				}
