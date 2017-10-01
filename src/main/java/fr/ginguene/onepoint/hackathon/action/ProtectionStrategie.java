d7a9670c66503bc99e6ea1b7400b58211efa391b
ginguene <ginguene@memnon>
2017-10-01 23:02:06 +0200

    Mode protection

--- src/main/java/fr/ginguene/onepoint/hackathon/action/ProtectionStrategie.java
+++ src/main/java/fr/ginguene/onepoint/hackathon/action/ProtectionStrategie.java
@@ -12,7 +12,7 @@ public class ProtectionStrategie extends AbstractStrategie {
 
 		int nbVaisseauEnnemie = carte.getNbVaisseauInFlotte(PlaneteStatus.Ennemie, source);
 
-		return (nbVaisseauEnnemie > source.getPopulation() / 2);
+		return (nbVaisseauEnnemie > 10);
 	}
 
 }
