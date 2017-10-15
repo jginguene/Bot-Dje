95b844af63ee4ca4e46e4d38da2367830a5c0438
ginguene <ginguene@memnon>
2017-10-15 21:35:32 +0200

    A

--- src/main/java/fr/ginguene/onepoint/hackathon/action/MegabombeStrategie.java
+++ src/main/java/fr/ginguene/onepoint/hackathon/action/MegabombeStrategie.java
@@ -40,10 +40,6 @@ public class MegabombeStrategie extends AbstractStrategie {
 
 		}
 
-		if (source.getPopulationMax() < 80) {
-			return false;
-		}
-
 		for (Planete aPlanete : carte.getPlanetesOrderByDistance(source)) {
 
 			if (aPlanete.getStatus() == PlaneteStatus.Ennemie) {
