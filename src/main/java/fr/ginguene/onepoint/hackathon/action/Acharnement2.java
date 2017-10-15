f330353bdb50e0a9e55a1bd97b14326707803629
ginguene <ginguene@memnon>
2017-10-15 22:14:06 +0200

    T

--- src/main/java/fr/ginguene/onepoint/hackathon/action/Acharnement2.java
+++ src/main/java/fr/ginguene/onepoint/hackathon/action/Acharnement2.java
@@ -1,5 +1,9 @@
 package fr.ginguene.onepoint.hackathon.action;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import fr.ginguene.onepoint.hackathon.Carte;
 import fr.ginguene.onepoint.hackathon.Flotte;
 import fr.ginguene.onepoint.hackathon.Planete;
@@ -7,24 +11,44 @@ import fr.ginguene.onepoint.hackathon.PlaneteStatus;
 import fr.ginguene.onepoint.hackathon.Response;
 import fr.ginguene.onepoint.hackathon.ordre.EnvoiFlotte;
 
-public class Acharnement extends AbstractStrategie {
+public class Acharnement2 extends AbstractStrategie {
 
 	private int destinationId = -1;
 
-	public Acharnement(boolean isDebug) {
+	private HashMap<Integer, Integer> map = new HashMap();
+	private HashMap<Integer, List<Integer>> reverseMap = new HashMap();
+
+	public Acharnement2(boolean isDebug) {
 		super(isDebug);
 	}
 
-	public Acharnement() {
+	public Acharnement2() {
 		super();
 	}
 
 	@Override
 	public boolean execute(Response response, Planete source, Carte carte, boolean isOptimizingScore) {
 
+		Integer destinationId = map.get(source.getId());
 		Planete destination = null;
 
-		if (destinationId > 0) {
+		if (destinationId == null) {
+
+			// Choisi une cible existante
+			for (Integer existingDestinationId : reverseMap.keySet()) {
+				if (reverseMap.get(existingDestinationId).size() < 4) {
+					destinationId = existingDestinationId;
+					map.put(source.getId(), existingDestinationId);
+					reverseMap.get(existingDestinationId).add(source.getId());
+					break;
+				}
+			}
+
+		}
+
+		// Controle de la cible
+		if (destinationId != null) {
+
 			destination = carte.getPlanete(destinationId);
 
 			boolean bombeEnApproche = false;
@@ -35,14 +59,19 @@ public class Acharnement extends AbstractStrategie {
 			}
 
 			if (destination.getStatus() == PlaneteStatus.Amie || bombeEnApproche) {
-				destination = null;
-				destinationId = -1;
+				reverseMap.remove(destinationId);
+				map.remove(source.getId());
+				destinationId = null;
 			}
 		}
 
-		if (destination == null) {
+		if (destinationId == null) {
 			destination = chooseTarget(source, carte);
 			destinationId = destination.getId();
+			reverseMap.put(destinationId, new ArrayList<>());
+			map.put(source.getId(), destinationId);
+		} else {
+			destination = carte.getPlanete(destinationId);
 		}
 
 		int nbVaisseau = source.getPopulation() - carte.getNbVaisseauInFlotte(PlaneteStatus.Ennemie, source, 20) - 10;
@@ -53,20 +82,18 @@ public class Acharnement extends AbstractStrategie {
 			response.addOrdre(ordre);
 			return true;
 		}
-
 		return false;
 	}
 
 	private Planete chooseTarget(Planete source, Carte carte) {
 
 		for (Planete aPlanete : carte.getPlanetesOrderByDistance(source)) {
-			if (aPlanete.getPopulationMax() > 40 && aPlanete.getTauxCroissance() > 1
-					&& aPlanete.getStatus() != PlaneteStatus.Amie) {
+			if (aPlanete.getTauxCroissance() > 1 && aPlanete.getStatus() != PlaneteStatus.Amie
+					&& !reverseMap.containsKey(aPlanete.getId())) {
 				return aPlanete;
 			}
 
 		}
-
 		// On choisit la planete ennemie la plus proche
 		return carte.getEnnemiLaPlusProche(source);
 
